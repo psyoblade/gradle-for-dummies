546859e53bab5c6a218d510e752c691d36e309d1
park.suhyuk <park.suhyuk@gmail.com>
2018-02-09 01:21:18 +0900

    Next, Chapter 2. mastering the fundamentals

---
+++ src/main/java/com/ncsoft/dataplatform/psyoblade/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.ncsoft.dataplatform.psyoblade;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HelloWorld {
+	private static final Logger logger = LoggerFactory.getLogger(HelloWorld.class);
+
+	public static void main(String[] args) {
+		logger.info("hello world");
+	}
+}
